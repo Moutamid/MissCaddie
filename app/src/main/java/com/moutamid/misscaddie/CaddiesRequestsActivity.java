951b0c3b5ed7b0d6291ac425901fe28ac369ef78
suleman <sunnybajwa904@gmail.com>
2022-10-12 22:19:51 +0500

    commit

---
+++ app/src/main/java/com/moutamid/misscaddie/CaddiesRequestsActivity.java
@@ -0,0 +1,14 @@
+package com.moutamid.misscaddie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaddiesRequestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caddies_requests);
+    }
+}
