a06cdacd458783dc10674fa88131fd4bd81c1756
suleman <sunnybajwa904@gmail.com>
2022-10-13 22:04:06 +0500

    commit

---
+++ app/src/main/java/com/moutamid/misscaddie/CaddieAvailabiltyActivity.java
@@ -0,0 +1,14 @@
+package com.moutamid.misscaddie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaddieAvailabiltyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caddie_availabilty);
+    }
+}
--- app/src/main/java/com/moutamid/misscaddie/Login_Caddie.java
+++ app/src/main/java/com/moutamid/misscaddie/Login_Caddie.java
@@ -2,13 +2,29 @@ package com.moutamid.misscaddie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class Login_Caddie extends AppCompatActivity {
+    TextView signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_caddie);
+
+        signInBtn = findViewById(R.id.signInBtn2);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login_Caddie.this , CaddieDeatilsActivity.class);
+                startActivity(intent);
+                Animatoo.animateZoom(Login_Caddie.this);
+            }
+        });
     }
 }
