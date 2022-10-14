7523d2d7ea05dc669b05474c53a3ad72d4a48a4b
suleman <sunnybajwa904@gmail.com>
2022-10-14 23:01:50 +0500

    commit

--- app/src/main/java/com/moutamid/misscaddie/Splash.java
+++ app/src/main/java/com/moutamid/misscaddie/Splash.java
@@ -21,7 +21,7 @@ public class Splash extends AppCompatActivity {
             @Override
             public void run()
             { //MainActivity
-                Intent homeIntent = new Intent(Splash.this, CaddieDashboardActivity.class);
+                Intent homeIntent = new Intent(Splash.this, MainActivity.class);
                 startActivity(homeIntent);
                 Animatoo.animateZoom(Splash.this);
                 finish();
