5e9fcb7f208722cfba571bd6e31a19aebb1f5e97
suleman <sunnybajwa904@gmail.com>
2022-10-14 21:31:32 +0500

    commit

---
+++ app/src/main/java/com/moutamid/misscaddie/CaddieEditProfileActivity.java
@@ -0,0 +1,14 @@
+package com.moutamid.misscaddie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaddieEditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caddie_edit_profile);
+    }
+}
